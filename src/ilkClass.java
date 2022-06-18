1f6ecc180907022c55d4889a8e569eb0217648de
Melekce <cakirmelek123@gmail.com>
2022-06-18 21:57:42 +0300

    ikinci değişiklik

--- src/ilkClass.java
+++ src/ilkClass.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ilkClass {
     public static void main(String[] args) {
         System.out.println("java kolay,github kolay hayat zor");
-        Scanner scan=new Scanner(System.in);
-        System.out.println("");
+        System.out.println("bu işide hallettik");
     }
 }
